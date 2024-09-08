410a07dc9d3d1ee5dd2ce98048aa4103c1ac034b
Chris Vander Mey <chris@pmpartner.ai>
2024-09-08 13:52:49 -0400

    updated setup.

---
+++ pp_sdk/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from api.api_api import ApiApi
+from api.chat_api import ChatApi
+from api.goals_api import GoalsApi
+from api.leaders_api import LeadersApi
+from api.prd_api import PrdApi
+from api.programs_api import ProgramsApi
+from api.userstory_api import UserstoryApi
+
