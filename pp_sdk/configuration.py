8ea03e00614d6f0b08494bdedcb89a227d65a175
Chris Vander Mey <chris@pmpartner.ai>
2024-09-10 12:19:29 -0400

    0.1.9

--- pp_sdk/configuration.py
+++ pp_sdk/configuration.py
@@ -385,7 +385,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.1.8".\
+               "SDK Package Version: 0.1.9".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
