33ca6327f8d9b366c2ee6cf3fb2fe59fe34dfba9
Chris Vander Mey <chris@pmpartner.ai>
2025-02-09 14:30:40 -0800

    Update SDK to version 0.4.4

--- pp_sdk/__init__.py
+++ pp_sdk/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 # import apis into sdk package
 from pp_sdk.api.chat_api import ChatApi
--- pp_sdk/configuration.py
+++ pp_sdk/configuration.py
@@ -535,7 +535,7 @@ conf = pp_sdk.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.4.3".\
+               "SDK Package Version: 0.4.4".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
