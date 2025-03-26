163d7861ec3e6b351ac23260ac8ca8733feae892
Chris Vander Mey <chris@pmpartner.ai>
2025-03-26 15:04:00 -0700

    Update SDK to version 0.5.2
    This is a clean update that removes all previous files and replaces them with the newly generated SDK.

--- pp_sdk/configuration.py
+++ pp_sdk/configuration.py
@@ -542,7 +542,7 @@ conf = pp_sdk.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.5.1".\
+               "SDK Package Version: 0.5.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
